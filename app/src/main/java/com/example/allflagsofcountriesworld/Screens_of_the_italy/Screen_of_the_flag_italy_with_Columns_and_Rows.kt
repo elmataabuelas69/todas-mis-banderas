abf7bb20631115990a5d1ffc4521a660949691a1
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-09-28 20:38:28 -0600

    Ya agregue la aguila a la bandera esta es la bandera creada de forma vertical aun falta la horizontal

--- app/src/main/java/com/example/allflagsofcountriesworld/Screens_of_the_italy/Screen_of_the_flag_italy_with_Columns_and_Rows.kt
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens_of_the_italy/Screen_of_the_flag_italy_with_Columns_and_Rows.kt
@@ -5,21 +5,31 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.allflagsofcountriesworld.ui.theme.green
 import com.example.allflagsofcountriesworld.ui.theme.red
+import androidx.compose.foundation.Image
+import com.example.allflagsofcountriesworld.R
+import com.example.allflagsofcountriesworld.ui.theme.white
 
 @Composable
 fun thescreen_italy_with_(modifier: Modifier = Modifier){
 Row(modifier= Modifier){
     Box(modifier=Modifier.fillMaxHeight().weight(1f).background(color=green))
-    Spacer(modifier= Modifier.fillMaxHeight().weight(1f)
-
+    Box(modifier= Modifier.fillMaxHeight().weight(1f).background(color= white)
     )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.aguilamexicana),
+                    contentDescription="Águila mexicana",
+            modifier=Modifier.fillMaxSize()
+
+            )
+    }
     Box(modifier=Modifier.fillMaxHeight().weight(1f).background(color=red))
 
 }
