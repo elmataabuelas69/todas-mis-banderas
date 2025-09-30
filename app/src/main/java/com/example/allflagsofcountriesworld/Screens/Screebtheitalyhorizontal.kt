5dee86938a1895ebe2203daf1d82e2eb90809c1b
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-09-30 08:04:42 -0600

    Ya hice la estructura principal de la bandera de forma horizontal y vertical de italia

--- app/src/main/java/com/example/allflagsofcountriesworld/Screens/Screebtheitalyhorizontal.kt
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/Screebtheitalyhorizontal.kt
@@ -2,26 +2,28 @@ package com.example.allflagsofcountriesworld.Screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun Flag_of_the_italy(modifier:Modifier= Modifier){
-Row(modifier= Modifier){
-    Box(modifier= Modifier.fillMaxHeight().weight(1f).background(Color.Green))
-    Spacer(modifier=Modifier.fillMaxHeight().weight(1f))
-    Box(modifier=Modifier.fillMaxHeight().weight(1f).background(Color.Red))
+fun Flag_horizontal_italy(modifier:Modifier= Modifier){
+    Column(modifier= Modifier){
+        Box(modifier= Modifier.fillMaxWidth().weight(1f).background(Color.Green))
+        Spacer(modifier=Modifier.fillMaxWidth().weight(1f))
+        Box(modifier=Modifier.fillMaxWidth().weight(1f).background(Color.Red))
 
-}
+    }
 }
 @Preview(showBackground = false, showSystemUi = true)
 @Composable
-fun showmyflag(){
-    Flag_of_the_italy(modifier=Modifier.fillMaxSize().background(Color.Black))
+fun showmyflagh(){
+    Flag_horizontal_italy(modifier=Modifier.fillMaxSize().background(Color.Black))
 }
