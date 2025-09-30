6d6da933043efd6a820c60fcc0593764d791c756
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-09-30 08:35:33 -0600

    Otro commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
 }
---
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/flag_horizontal_of_japan.kt
@@ -0,0 +1,8 @@
+package com.example.allflagsofcountriesworld.Screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun japan_flag_horizontal(){
+
+}
