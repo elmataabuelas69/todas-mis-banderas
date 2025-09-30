62ff71b730796fd42ebcd943460cb8a5911b7e83
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-09-30 08:35:33 -0600

    Otro commit

---
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/Flagofbrasil.kt
@@ -0,0 +1,50 @@
+package com.example.allflagsofcountriesworld.Screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Canvas
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+
+@Composable
+fun brasil(modifier: Modifier =Modifier){
+    Column(modifier= Modifier) {
+
+        Canvas(modifier = modifier.fillMaxSize()) {
+            val centerX = size.width / 2
+            val centerY = size.height / 2
+            val halfWidth = size.width / 2
+            val halfHeight = size.height / 2
+
+            val path = Path().apply {
+                moveTo(centerX, 0f)
+                lineTo(size.width, centerY)
+                lineTo(centerX, size.height)
+                lineTo(0f, centerY)
+                close()
+            }
+
+            drawPath(
+                path,
+                color = Color.Yellow,
+                style = Fill
+            )
+        }
+
+
+    }
+}
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+fun showbrasilflag(){
+    brasil(modifier=Modifier.fillMaxSize().background(Color.Green))
+}
