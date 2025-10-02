565ee677cd1ab4429a067ade542a1cc26dab3a6c
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-10-02 15:56:46 -0600

    agregue unas cuantas funciones para construir las banderas con layouts

---
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/Screenoftheitalyverticalwithlayout.kt
@@ -0,0 +1,15 @@
+package com.example.allflagsofcountriesworld.Screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun horizonatlflag_oftheitaltwithlayouts(modifier: Modifier=Modifier){
+
+}
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+fun showscreenmyflagwithlayout(){
+
+}
