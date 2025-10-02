e58449ccb8d5c46cf5451b5bb1df536cd53cdaf3
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-10-02 16:21:46 -0600

    agregue unas cuantas funciones para construir las banderas con layouts

--- app/src/main/java/com/example/allflagsofcountriesworld/Screens/Screenoftheitalyverticalwithlayout.kt
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/Screenoftheitalyverticalwithlayout.kt
@@ -1,12 +1,26 @@
 package com.example.allflagsofcountriesworld.Screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun horizonatlflag_oftheitaltwithlayouts(modifier: Modifier=Modifier){
-
+    ConstraintLayout(modifier= Modifier){
+        val (box1, box2, box3)=createRefs()
+        Box(modifier=Modifier.size(size=200.dp).constrainAs(ref=box1){
+        start.linkTo(anchor = parent.start)
+            end.linkTo(anchor=parent.end)
+            top.linkTo(anchor = parent.top)
+            bottom.linkTo(anchor = parent.top)
+            height=ConstrainsToFill();
+                width=
+        })
+    }
 }
 @Preview(showBackground = false, showSystemUi = true)
 @Composable
