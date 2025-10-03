9f95db93f309ca3c385952ecea25d50bc2a515db
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-10-03 03:05:58 -0600

    Banderadechileconlayouts

--- app/src/main/java/com/example/allflagsofcountriesworld/Screens/Banderadechileconlayouts.kt
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/Banderadechileconlayouts.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
