ce4782ed871089cb7919f7dc705292344f2fd0db
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-10-02 22:18:14 -0600

    Termine la bandera de japon de forma horizontal con constrain layouts

---
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/Screenofthejapanvertical.kt
@@ -0,0 +1,57 @@
+package com.example.allflagsofcountriesworld.Screens
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import java.util.Arrays.fill
+import kotlin.io.path.Path
+
+@Composable
+fun screenofthejapanvertical(modifier:Modifier= Modifier){
+    ConstraintLayout(modifier=modifier){
+        val (box1, circle)=createRefs()
+        Box(modifier= Modifier.background(Color.White).constrainAs(ref=box1){
+        start.linkTo(parent.start)
+        end.linkTo((parent.end))
+        top.linkTo((parent.top))
+        bottom.linkTo((parent.bottom))
+        width= Dimension.fillToConstraints
+            height= Dimension.fillToConstraints
+        })
+        Canvas(modifier = Modifier.size(200.dp).constrainAs(ref=circle){
+            start.linkTo(parent.start)
+            end.linkTo((parent.end))
+            top.linkTo((parent.top))
+            bottom.linkTo((parent.bottom))
+        }) {
+            drawCircle(
+                color = Color.Red,
+                radius = 280f,
+                center = Offset(size.width / 2, size.height / 2),
+                style = Fill
+            )
+        }
+
+
+    }
+}
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+fun showscreen(){
+    screenofthejapanvertical(modifier = Modifier.fillMaxSize().background(Color.Green))
+}
