f4aa86460cb5566c4cc445338264daeb2512f8bf
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-10-09 01:30:14 -0600

    Pixel art terminado

--- app/src/main/java/com/example/allflagsofcountriesworld/Screens/mipixelart.kt
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/mipixelart.kt
@@ -2,13 +2,16 @@ package com.example.allflagsofcountriesworld.Screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,8 +23,9 @@ import com.example.allflagsofcountriesworld.ui.theme.color19
 import com.example.allflagsofcountriesworld.ui.theme.color20
 
 @Composable
-fun mipixel_art(modifier: Modifier= Modifier.fillMaxHeight()) {
-    Column(modifier = Modifier) {
+fun mipixel_art(modifier: Modifier= Modifier) {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center
+    ) {
 
         Row(modifier=Modifier){
             repeat(20) {
