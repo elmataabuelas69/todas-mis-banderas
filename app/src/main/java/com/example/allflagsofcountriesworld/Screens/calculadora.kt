e6b7c1a342f36ada849eb52815dd193e4c1ebc42
elmataabuelas69 <rafaelmorenov12@gmail.com>
2025-10-14 00:58:45 -0600

    Merge remote-tracking branch 'origin/bandera_de_brasil' into bandera_de_brasil

--- app/src/main/java/com/example/allflagsofcountriesworld/Screens/calculadora.kt
+++ app/src/main/java/com/example/allflagsofcountriesworld/Screens/calculadora.kt
@@ -2,17 +2,24 @@ package com.example.allflagsofcountriesworld.Screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.constraintlayout.compose.Dimension.Companion.value
+var numero1 by rememberSaveable() { mutableSetOf("") }
 @Composable
 fun mycalc(modifier: Modifier= Modifier){
-   value: TextFieldValue,
-   miValueChange:(TextFieldValue)
+
+
 }
 
+@Composable
+fun numeros(modifier: Modifier= Modifier){
+
+}
 @Preview(showBackground = false, showSystemUi = true)
 @Composable
 fun showmic(){
